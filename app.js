9e64851948e748a45ffa2f64f971802da3bbbac9
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-18 20:00:01 +0600

    Question and answer project design and add some question

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
  let app = Vue.createApp({
     data(){
         return {
-            
-            
+            modelTest: [
+                {quiz: "Which is correct for Vue js ?", ans: 'Framework'},
+                {quiz: "Vue is created by ?", ans: 'javascript'},
+                {quiz: "Who is vue invented ?", ans: 'evan you'},
+            ],
+            option: [
+                {a: "Library", b: "Framework"},
+            ]
         }
     },
     methods: {
