30675e524d1726073b82f3c40100f19bc1e7d0f2
A-Al-Mamun <abdullahalmamun3073@gmail.com>
2024-01-22 23:38:50 +0600

    Result checking multiple

--- app.js
+++ app.js
@@ -1,34 +1,62 @@
  let app = Vue.createApp({
     data(){
         return {
-            isCorrect: null,
+            isCorrect: "",
             myClass: "",
-            A: null,
             modelTest: [
-                {
+                quiz1 = {
                     quiz: "Which is correct for Vue js ?", 
-                    ans: 'Framework'
-                },
-            ],
-            option: [
-                {
+                    ans: 'Framework',
                     A: 'Library', 
                     B: 'Framework', 
                     C: 'Operation',
                     D: 'Software'
-                }
+                },
+                quiz2 = {
+                    quiz: "Who is invented Vue ?", 
+                    ans: 'Evan You',
+                    A: 'Gates', 
+                    B: 'Brenden', 
+                    C: 'Evan You',
+                    D: 'A.A.Mamun'
+                },
             ]
+            // modelTest: [
+            //     {
+            //         quiz: "Which is correct for Vue js ?", 
+            //         ans: 'Framework',
+            //         A: 'Library', 
+            //         B: 'Framework', 
+            //         C: 'Operation',
+            //         D: 'Software'
+            //     },
+            //     {
+            //         quiz: "Who is invented Vue ?", 
+            //         ans: 'Evan You',
+            //         A: 'Gates', 
+            //         B: 'Brenden', 
+            //         C: 'Evan You',
+            //         D: 'A.A.Mamun'
+            //     },
+            // ],
+            
         }
     },
     methods: {
         quizAns(e) {
-            for(item in this.modelTest){
+            // for(item in this.modelTest){
                 if(e == 'Framework'){
-                    console.log("True")
-                }else {
-                    console.log("False");
+                    this.isCorrect = "Correct";
+                    // this.isCorrect = e.target.value;
+                    // this.isCorrect = this.modelTest[item].ans;
+                }else if(e == 'Evan You'){
+                    this.isCorrect = "Correct";
+                    // this.isCorrect = this.modelTest[item].ans;
                 }
-            }
+                else {
+                    this.isCorrect = "False";
+                }
+            // }
         },
     },
     watch: {
